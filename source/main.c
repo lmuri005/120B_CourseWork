5f72d4a19f1a0506320a70983f3c69ba02a3a608
Leonardo Murillo <lmuri005@wch136-02.cs.ucr.edu>
2020-01-10 14:42:49 -0800

    Puto

--- source/main.c
+++ source/main.c
@@ -19,11 +19,25 @@ int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA=0x00; PORTA=0xFF;
 	DDRB=0xFF; PORTB=0x00;	
+         unsigned char PUTOA0=(PINA & 0xFE);
+        unsigned char PUTOA1=(PINA & 0xFD);
     /* Insert your solution below */
-	unsigned char temp_input=0x00;
+	//unsigned char temp_input=0x00;
+
     while (1) {
-	temp_input=PINA;
-	PORTB=temp_input;
+	//temp_input=PINA;
+	//PORTB=temp_input;
+	 
+	if((PUTOA0==0x00&&PUTOA1==0x00)){
+	PORTB=0x00;
+    } else if((PUTOA1==0x00&&PUTOA0==0x01)){
+        PORTB=0X01;
+
+    }else if((PUTOA1==0X01&&PUTOA0==0x00)){
+	PORTB=0X00;
+    }else if((PUTOA1==0x01&&PUTOA0==0x01)){
+	PORTB=0x00;
+}
 
     }
     return 1;
