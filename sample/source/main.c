1e7db857cd679453595e1098e0005de6f934103b
Leonardo Murillo <lmuri005@wch136-03.cs.ucr.edu>
2020-01-10 16:42:09 -0800

    lab2

---
+++ sample/source/main.c
@@ -0,0 +1,31 @@
+/*	Author: lmuri005
+ *  Partner(s) Name: none
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+DDRA=0x00;
+DDRB=0xFF;
+PINA=0xFF;
+PORTB=0x00;
+
+    /* Insert your solution below */
+    while (1) {
+	if(PINA==0xFE){
+	PORTB=0xFF;	
+}else if(PINA==0xFF){
+	PORTB=0x00;
+}
+    }
+    return 1;
+}
