8ed99d6a99eb785544f9c946bf0da90221546a48
Leonardo Murillo <lmuri005@wch136-03.cs.ucr.edu>
2020-01-14 12:21:29 -0800

    Working Progress

--- lab3/source/main.c
+++ lab3/source/main.c
@@ -15,12 +15,22 @@
 int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA=0x00;
-	DDRB=0xFF;
+	DDRB=0x00;
+	DDRC=0xFF;
 	PORTA=0xFF;
-	PORTB=0x00;
+	PORTB=0xFF;
+	PORTC=0x00;
+	unsigned char PenA1;
+	unsigned char PenB1;
+	unsigned char counter;
     /* Insert your solution below */
-    while (1) {
+    while (1) 
+	PenA1=PORTA&0x01;
+	if((PenA1==0x01)||(PenB1==0x01)){
+	counter ++;
+}
 
+PORTC=counter; 
     }
     return 1;
 }
