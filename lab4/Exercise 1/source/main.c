d47b7a755e3e13f7211b9634b9ceb032b8e29022
Leonardo Murillo <lmuri005@wch136-03.cs.ucr.edu>
2020-01-18 15:41:31 -0800

    Still working on it

--- lab4/Exercise 1/source/main.c
+++ lab4/Exercise 1/source/main.c
@@ -12,15 +12,42 @@
 #include "simAVRHeader.h"
 #endif
 
-int main(void) {
+
+//enum States{sm_start,not_pressed,pressed}state;	// list of states of the system
+void Tick()
+{
+
+switch(state){
+	case start:
+	state=not_pressed;
+	break;
+
+	defualt:
+	state=start;
+}
+switch(state){
+	case not_pressed:
+	PORTB=0x01
+	break;
+	case pressed:
+	PORTB=0x02;
+	break;
+}
+
+}
+
+
+
+int main() {
     /* Insert DDR and PORT initializations */
+
 	DDRA=0x00;
 	DDRB=0xFF;
 	PORTA=0xFF;
 	PORTB=0x00;
     /* Insert your solution below */
     while (1) {
-
+//	Tick();
     }
-    return 1;
+//    return 1;
 }
