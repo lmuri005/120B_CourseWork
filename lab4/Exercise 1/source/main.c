12b66690f89a5d1c49f1829a76e3ff5ff225e1c3
Leonardo Murillo <lmuri005@wch136-03.cs.ucr.edu>
2020-01-18 13:26:36 -0800

    STARTED

---
+++ lab4/Exercise 1/source/main.c
@@ -0,0 +1,26 @@
+/*	Author: lmuri005
+ *  Partner(s) Name: Emilio
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+	DDRA=0x00;
+	DDRB=0xFF;
+	PORTA=0xFF;
+	PORTB=0x00;
+    /* Insert your solution below */
+    while (1) {
+
+    }
+    return 1;
+}
